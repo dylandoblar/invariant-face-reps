8e334754f7e7c61eaba117152ef7b6dd1177c5e6
katiemc <kcollins@Katies-iMac.local>
2020-12-14 23:58:45 -0500

    rotation exploration

--- eval.py
+++ eval.py
@@ -6,6 +6,8 @@ from utils import *
 from sklearn.metrics import classification_report,f1_score,matthews_corrcoef,auc
 import shutil
 
+# note to katie: run "source env/bin/activate" to start this virtualenv
+
 def evaluate(model, dataset, num_samples=-1,logging_dir=None):
     '''
     Evaluates the model on pairs of examples in the dataset specified by dataset_path.
@@ -230,144 +232,221 @@ def run_real_data_exp(repr_type, data_type, vgg_model_type=None, num_ids=50, num
 
     return logging_dir
 
+def run_rot_data_exp(repr_type, template_dir, test_dir, logging_dir, vgg_model_type=None, num_ids=50, num_per_id=15, num_pca_dim=50,sample_complexity_exp=False):
+    #random.seed(7)
+
+    if sample_complexity_exp:
+        logging_dir = f'{logging_dir}/sample_complexity'
+
+    if not os.path.exists(logging_dir):os.makedirs(logging_dir)
+    # else:
+    #     print("skipping: ", logging_dir)
+    #     return logging_dir  # don't re-run experiment if directory exists (change if need to rerun)
+
+    print("template: ", template_dir)
+    print("test: ", test_dir)
+
+    # create model
+    model = TemplateModel(
+        template_dir,
+        repr_type=repr_type,
+        pca_dim=num_pca_dim,
+        standardize=True,
+        num_thresh_samples=500,
+        num_template_ids=num_ids,
+        num_template_samples_per_id=num_per_id,
+        vgg_model_path=f'vgg_model_{vgg_model_type}.h5',
+        logging_dir=logging_dir,
+    )
+
+    dataset = load_dataset(test_dir,keep_file_names=True)
+    acc = evaluate(model, dataset, num_samples=100,logging_dir=logging_dir)
+    print(f"model accuracy on the balanced test set : {acc}")
+
+    # if not sample_complexity_exp:
+    #     # only run tsne for a single run to save time
+    #     num_ids = 10
+    #     data_subset = load_dataset(test_dir, num_ids=num_ids, num_samples_per_id=0, shuffle=True,keep_file_names=True)
+    #     title = f'{repr_type}-Model Features: Real Data'
+    #     emb_data = compute_tsne(model, data_subset, logging_dir, title, use_raw_features = False, num_classes=num_ids)
+    #     title = f'{repr_type} Raw Features: Real Data'
+    #     emb_data = compute_tsne(model, data_subset, logging_dir, title, use_raw_features = True, num_classes=num_ids)
+
+    return logging_dir
+
+
 if __name__ == '__main__':
 
-    final_output_dir = "./final_final_plots/"
+    final_output_dir = "./rot_plots/"
     if not os.path.exists(final_output_dir): os.makedirs(final_output_dir)
 
     do_tsne = False
     tsne_title=[None, None] # depending on presentation mode, don't add title
 
-    repr_types = ["HOG", "VGG"]
-    vgg_model_types = ["extreme", "normal", "pretrain"]
-    poss_templates = ["normal", "extreme"]
-    poss_tests = ["normal", "extreme"]
-    styles = ['-b*', '-ro', '-gs', '-ph']
+    rot_data_dir = f'/Users/kcollins/invariant_face_data/rot_data/'
+    main_logging_dir = f'./rot_logging_dir/'
+    styles = ['b-*', 'r-o', 'g--', 'p-*', 'm-o', 'c--', 'y-*', 'r-^', 'k-o', 'g-*']
     all_metrics = ["mcc", "auc", "accuracy", "f1_score"]
 
-    # 1) HOG vs. VGG: normal, normal
-    template_data = "normal"
-    test_data = "normal"
-    vgg_model_type = "pretrain"
-    logging_dirs = []
-    for repr_type in repr_types:
-        logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
-        logging_dirs.append(logging_dir)
-    labels = ["HOG", "VGG-Face"]
-    file_tag = "hog_vgg_normal_normal.png"
-    title = "HOG vs. VGG-Face: Natural Illumination"
-    create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
-    if do_tsne:
-        # run_tsne("HOG Features t-SNE: Natural Illumination", final_output_dir, "HOG", template_data, test_data, vgg_model_type)
-        # run_tsne("VGG-Face Features t-SNE: Natural Illumination", final_output_dir, "VGG", template_data, test_data, vgg_model_type)
-        run_tsne(tsne_title, final_output_dir, "HOG", template_data, test_data,
-                 vgg_model_type)
-        run_tsne(tsne_title, final_output_dir, "VGG", template_data, test_data,
-                 vgg_model_type)
+    data_types = ['ill_normal_rot_yaw_-90_90_uniform', 'rot_yaw_-45_45_uniform', 'rot_yaw_-90_90_uniform']
+    data_names = ["Coupled", "Natural", "Extreme"]
+    repr_type = "HOG"
 
-    # 2) VGG varieties -- compare training under diff settings
-    repr_type = "VGG"
-    for template_data, template_name in zip(poss_templates, ["Natural", "Extreme"]):
-        for test_data, test_name in zip(poss_tests, ["Natural", "Extreme"]):
-            logging_dirs = []
-            for vgg_model_type, vgg_model_name in zip(vgg_model_types, ["Extreme", "Natural", "No Fine-Tuning"]):
-                logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
-                logging_dirs.append(logging_dir)
-            labels = ["Extreme", "Natural", "No Fine-Tuning"]
-            file_tag = f'all_vgg_{template_data}_{test_data}.png'
-            title = f'{template_name} Templates, {test_name} Test'
-            create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
-
-    # 3) HOG vs. VGG: generalizing to extreme data
-    vgg_model_type = "extreme"
-    test_data = "extreme"
     logging_dirs = []
-    for template_data in poss_templates:
-        for repr_type in repr_types:
-            logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
+    labels = []
+    num_ids = 100
+    num_per_id = 30
+    for template_data, template_name in zip(data_types, data_names):
+        for test_data, test_name in zip(data_types, data_names):
+            template_dir = f'{rot_data_dir}{template_data}_template/img'
+            test_dir = f'{rot_data_dir}{test_data}_test/img'
+            logging_dir = f'{main_logging_dir}/{template_name}_{test_name}/'
+            logging_dir = run_rot_data_exp(repr_type, template_dir, test_dir, logging_dir,num_ids=num_ids, num_per_id=num_per_id)
             logging_dirs.append(logging_dir)
-    file_tag = f'hog_vgg_all_{test_data}.png'
-    title = f'HOG vs. VGG-Face: Extreme Illumination'
-    labels = ["HOG (Natural)", "VGG-Face (Natural)", "HOG (Extreme)", "VGG-Face (Extreme)"]
+            labels.append(f'({template_name}, {test_name})')
+    file_tag = f'all_new_rot_exps.png'
+    title = f'Explore Rotation'
+    #title = f'{template_name} Templates, {test_name} Test'
     create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
-    if do_tsne:
-        # run_tsne("HOG Features t-SNE: Natural Illumination", final_output_dir, "HOG", "normal", test_data, vgg_model_type)
-        # run_tsne("VGG-Face Features t-SNE: Natural Illumination", final_output_dir, "VGG", "normal", test_data, vgg_model_type)
-        # run_tsne("HOG Features t-SNE: Extreme Illumination", final_output_dir, "HOG", "extreme", test_data,
-        #          vgg_model_type)
-        # run_tsne("VGG-Face Features t-SNE: Extreme Illumination", final_output_dir, "VGG", "extreme", test_data,
-        #          vgg_model_type)
-        run_tsne(tsne_title, final_output_dir, "HOG", "normal", test_data, vgg_model_type)
-        run_tsne(tsne_title, final_output_dir, "VGG", "normal", test_data, vgg_model_type)
-        run_tsne(tsne_title, final_output_dir, "HOG", "extreme", test_data,
-                 vgg_model_type)
-        run_tsne(tsne_title, final_output_dir, "VGG", "extreme", test_data,
-                 vgg_model_type)
-    # sample complexity
-    test_data = "extreme"
-    vgg_model_type = "extreme"
 
-    all_num_ids = [5, 10, 25, 50, 100]
-    all_samp_per_id = [5,10,15,25,50]
-    num_pca_dim = 25
-    for repr_type in repr_types:
-        for template_data in poss_templates:
-            logging_dirs = []
-            for num_ids in all_num_ids:
-                for num_samp_per_id in all_samp_per_id:
-                    print("running: ", num_ids, num_samp_per_id)
-                    logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type, num_ids, num_samp_per_id, num_pca_dim,
-                                                 True)
-                    logging_dirs.append(logging_dir)
-            for metric in ["auc", "f1_score"]:
-                file_tag = f'./final_final_plots/{metric}_sample_complexity_heatmap_{repr_type}_{template_data}_{test_data}.png'
-                title = None
-                complexity_heatmap(logging_dirs, all_num_ids, all_samp_per_id, title, file_tag, metric)
-
-            #run sample complexity exp (num_ids, num_per_id)
-            settings = [(15,50), (30,25), (25,30), (50,15), (150,5),(250,3)]
-            for repr_type, model_name in zip(repr_types, ["HOG", "VGG-Face"]):
-                labels = []
-                logging_dirs = []
-                for num_ids, num_per_id in settings:
-                    print("running: ", num_ids, num_per_id)
-                    logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type, num_ids, num_per_id, sample_complexity_exp=True)
-                    logging_dirs.append(logging_dir)
-                    labels.append(f'Num IDs: {num_ids}, Num Per ID: {num_per_id}')
-                styles = ['b-*', 'r-o', 'g--', 'p-*', 'm-o', 'c--', 'y-*']
-                file_tag = f'./final_final_plots/sample_complexity_{repr_type}_{template_data}_{test_data}.png'
-                title = f'{model_name}'
-                create_overlayed_rocs(title, labels, styles, logging_dirs, file_tag)
-
-    # 4) HOG vs. VGG: real-data
-    num_ids = 20
-    num_per_id = 15
-    num_pca_dim = 50
-    data_type = "pubfig83"
-    vgg_model_type = "pretrain"
-    logging_dirs = []
-    for repr_type in repr_types:
-        logging_dir = run_real_data_exp(repr_type, data_type, vgg_model_type, num_ids, num_per_id, num_pca_dim)
-        logging_dirs.append(logging_dir)
-    labels = ["HOG", "VGG"]
-    file_tag = "hog_vgg_pubfig83.png"
-    title = "HOG vs. VGG-Face: Real-World Images"
-    create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
-    if do_tsne:
-        run_tsne(["HOG Projected Features", "HOG Raw Features"], final_output_dir, "HOG", data_type, data_type, vgg_model_type,num_ids, num_per_id, num_pca_dim)
-        run_tsne(["VGG-Face Projected Features", "VGG-Face Raw Features"], final_output_dir, "VGG", data_type, data_type, vgg_model_type,num_ids, num_per_id, num_pca_dim)
-
-    # 5) Baseline comparison against random features
-    repr_type = "RANDOM"
-    vgg_model_type = "pretrain" # doesn't matter
-    logging_dirs = []
-    for template_data, template_name in zip(poss_templates, ["Natural", "Extreme"]):
-        for test_data, test_name in zip(poss_tests, ["Natural", "Extreme"]):
-            logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
-            logging_dirs.append(logging_dir)
-    labels = ["Natural Templates, Natural Test", "Natural Templates, Extreme Test",
-              "Extreme Templates, Natural Test", "Extreme Templates, Extreme Test"]
-    file_tag = f'all_random.png'
-    title = f'Random Feature Vectors'
-    create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
+# if __name__ == '__main__':
+#
+#     final_output_dir = "./final_final_plots/"
+#     if not os.path.exists(final_output_dir): os.makedirs(final_output_dir)
+#
+#     do_tsne = False
+#     tsne_title=[None, None] # depending on presentation mode, don't add title
+#
+#     repr_types = ["HOG", "VGG"]
+#     vgg_model_types = ["extreme", "normal", "pretrain"]
+#     poss_templates = ["normal", "extreme"]
+#     poss_tests = ["normal", "extreme"]
+#     styles = ['-b*', '-ro', '-gs', '-ph']
+#     all_metrics = ["mcc", "auc", "accuracy", "f1_score"]
+#
+#     # 1) HOG vs. VGG: normal, normal
+#     template_data = "normal"
+#     test_data = "normal"
+#     vgg_model_type = "pretrain"
+#     logging_dirs = []
+#     for repr_type in repr_types:
+#         logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
+#         logging_dirs.append(logging_dir)
+#     labels = ["HOG", "VGG-Face"]
+#     file_tag = "hog_vgg_normal_normal.png"
+#     title = "HOG vs. VGG-Face: Natural Illumination"
+#     create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
+#     if do_tsne:
+#         # run_tsne("HOG Features t-SNE: Natural Illumination", final_output_dir, "HOG", template_data, test_data, vgg_model_type)
+#         # run_tsne("VGG-Face Features t-SNE: Natural Illumination", final_output_dir, "VGG", template_data, test_data, vgg_model_type)
+#         run_tsne(tsne_title, final_output_dir, "HOG", template_data, test_data,
+#                  vgg_model_type)
+#         run_tsne(tsne_title, final_output_dir, "VGG", template_data, test_data,
+#                  vgg_model_type)
+#
+#     # 2) VGG varieties -- compare training under diff settings
+#     repr_type = "VGG"
+#     for template_data, template_name in zip(poss_templates, ["Natural", "Extreme"]):
+#         for test_data, test_name in zip(poss_tests, ["Natural", "Extreme"]):
+#             logging_dirs = []
+#             for vgg_model_type, vgg_model_name in zip(vgg_model_types, ["Extreme", "Natural", "No Fine-Tuning"]):
+#                 logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
+#                 logging_dirs.append(logging_dir)
+#             labels = ["Extreme", "Natural", "No Fine-Tuning"]
+#             file_tag = f'all_vgg_{template_data}_{test_data}.png'
+#             title = f'{template_name} Templates, {test_name} Test'
+#             create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
+#
+#     # 3) HOG vs. VGG: generalizing to extreme data
+#     vgg_model_type = "extreme"
+#     test_data = "extreme"
+#     logging_dirs = []
+#     for template_data in poss_templates:
+#         for repr_type in repr_types:
+#             logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
+#             logging_dirs.append(logging_dir)
+#     file_tag = f'hog_vgg_all_{test_data}.png'
+#     title = f'HOG vs. VGG-Face: Extreme Illumination'
+#     labels = ["HOG (Natural)", "VGG-Face (Natural)", "HOG (Extreme)", "VGG-Face (Extreme)"]
+#     create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
+#     if do_tsne:
+#         # run_tsne("HOG Features t-SNE: Natural Illumination", final_output_dir, "HOG", "normal", test_data, vgg_model_type)
+#         # run_tsne("VGG-Face Features t-SNE: Natural Illumination", final_output_dir, "VGG", "normal", test_data, vgg_model_type)
+#         # run_tsne("HOG Features t-SNE: Extreme Illumination", final_output_dir, "HOG", "extreme", test_data,
+#         #          vgg_model_type)
+#         # run_tsne("VGG-Face Features t-SNE: Extreme Illumination", final_output_dir, "VGG", "extreme", test_data,
+#         #          vgg_model_type)
+#         run_tsne(tsne_title, final_output_dir, "HOG", "normal", test_data, vgg_model_type)
+#         run_tsne(tsne_title, final_output_dir, "VGG", "normal", test_data, vgg_model_type)
+#         run_tsne(tsne_title, final_output_dir, "HOG", "extreme", test_data,
+#                  vgg_model_type)
+#         run_tsne(tsne_title, final_output_dir, "VGG", "extreme", test_data,
+#                  vgg_model_type)
+#     # sample complexity
+#     test_data = "extreme"
+#     vgg_model_type = "extreme"
+#
+#     all_num_ids = [5, 10, 25, 50, 100]
+#     all_samp_per_id = [5,10,15,25,50]
+#     num_pca_dim = 25
+#     for repr_type in repr_types:
+#         for template_data in poss_templates:
+#             logging_dirs = []
+#             for num_ids in all_num_ids:
+#                 for num_samp_per_id in all_samp_per_id:
+#                     print("running: ", num_ids, num_samp_per_id)
+#                     logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type, num_ids, num_samp_per_id, num_pca_dim,
+#                                                  True)
+#                     logging_dirs.append(logging_dir)
+#             for metric in ["auc", "f1_score"]:
+#                 file_tag = f'./final_final_plots/{metric}_sample_complexity_heatmap_{repr_type}_{template_data}_{test_data}.png'
+#                 title = None
+#                 complexity_heatmap(logging_dirs, all_num_ids, all_samp_per_id, title, file_tag, metric)
+#
+#             #run sample complexity exp (num_ids, num_per_id)
+#             settings = [(15,50), (30,25), (25,30), (50,15), (150,5),(250,3)]
+#             for repr_type, model_name in zip(repr_types, ["HOG", "VGG-Face"]):
+#                 labels = []
+#                 logging_dirs = []
+#                 for num_ids, num_per_id in settings:
+#                     print("running: ", num_ids, num_per_id)
+#                     logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type, num_ids, num_per_id, sample_complexity_exp=True)
+#                     logging_dirs.append(logging_dir)
+#                     labels.append(f'Num IDs: {num_ids}, Num Per ID: {num_per_id}')
+#                 styles = ['b-*', 'r-o', 'g--', 'p-*', 'm-o', 'c--', 'y-*']
+#                 file_tag = f'./final_final_plots/sample_complexity_{repr_type}_{template_data}_{test_data}.png'
+#                 title = f'{model_name}'
+#                 create_overlayed_rocs(title, labels, styles, logging_dirs, file_tag)
+#
+#     # 4) HOG vs. VGG: real-data
+#     num_ids = 20
+#     num_per_id = 15
+#     num_pca_dim = 50
+#     data_type = "pubfig83"
+#     vgg_model_type = "pretrain"
+#     logging_dirs = []
+#     for repr_type in repr_types:
+#         logging_dir = run_real_data_exp(repr_type, data_type, vgg_model_type, num_ids, num_per_id, num_pca_dim)
+#         logging_dirs.append(logging_dir)
+#     labels = ["HOG", "VGG"]
+#     file_tag = "hog_vgg_pubfig83.png"
+#     title = "HOG vs. VGG-Face: Real-World Images"
+#     create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
+#     if do_tsne:
+#         run_tsne(["HOG Projected Features", "HOG Raw Features"], final_output_dir, "HOG", data_type, data_type, vgg_model_type,num_ids, num_per_id, num_pca_dim)
+#         run_tsne(["VGG-Face Projected Features", "VGG-Face Raw Features"], final_output_dir, "VGG", data_type, data_type, vgg_model_type,num_ids, num_per_id, num_pca_dim)
+#
+#     # 5) Baseline comparison against random features
+#     repr_type = "RANDOM"
+#     vgg_model_type = "pretrain" # doesn't matter
+#     logging_dirs = []
+#     for template_data, template_name in zip(poss_templates, ["Natural", "Extreme"]):
+#         for test_data, test_name in zip(poss_tests, ["Natural", "Extreme"]):
+#             logging_dir = run_experiment(repr_type, template_data, test_data, vgg_model_type)
+#             logging_dirs.append(logging_dir)
+#     labels = ["Natural Templates, Natural Test", "Natural Templates, Extreme Test",
+#               "Extreme Templates, Natural Test", "Extreme Templates, Extreme Test"]
+#     file_tag = f'all_random.png'
+#     title = f'Random Feature Vectors'
+#     create_overlayed_rocs(title, labels, styles, logging_dirs, final_output_dir + file_tag)
 
